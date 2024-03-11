144262d5428d311639ddbce5a78740e72a83dd63
Manasasudireddy <2110039472@klh.edu.in>
2024-03-11 19:18:14 +0530

    first commit

---
+++ src/main/java/com/business/entities/Product.java
@@ -0,0 +1,63 @@
+package com.business.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pid;
+    private String pname;
+    private double pprice;
+    private String pdescription;
+    private String imageFileName; // New field for storing image filename
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public double getPprice() {
+        return pprice;
+    }
+
+    public void setPprice(double pprice) {
+        this.pprice = pprice;
+    }
+
+    public String getPdescription() {
+        return pdescription;
+    }
+
+    public void setPdescription(String pdescription) {
+        this.pdescription = pdescription;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
+    }
+
+    public void setImageFileName(String imageFileName) {
+        this.imageFileName = imageFileName;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [pid=" + pid + ", pname=" + pname + ", pprice=" + pprice + ", pdescription=" + pdescription
+                + ", imageFileName=" + imageFileName + "]";
+    }
+}
